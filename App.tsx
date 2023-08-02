4ea59935f92ce458ce64f84b43f6b4d1abaf9f66
IdlyBond <53187706+IdlyBond@users.noreply.github.com>
2023-08-02 23:47:48 +0100

    feat: position change try

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
-import React, {useEffect} from 'react'
-import {StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
-
+import React, {useEffect, useState} from 'react'
+import {StyleSheet, Text, View, ImageBackground, Image, Dimensions} from 'react-native';
 export default function App() {
-    return (
 
+
+    const [characterPosition, setCharacterPosition] =  useState(0)
+    
+    useEffect(() => {
+        let pos = 0
+        setCharacterPosition(0)
+        const interval =  setInterval(() => {
+            pos += 1;
+            if (pos >= Dimensions.get('window').height / 2) {
+                pos = 0;
+            }
+            setCharacterPosition(pos);
+            console.log("New Iteration " + pos)
+        }, 30)
+        return () => clearInterval(interval);
+    }, [])
+    return (
         <ImageBackground
             source={require('./assets/background.png')}
             style={styles.backgroundImage}>
             <Image
-                source={require('../assets/character.png')}
-                style={styles.character}
+                source={require('./assets/character.png')}
+                style={[styles.character, {top: characterPosition}]}
             ></Image>
         </ImageBackground>
     );
