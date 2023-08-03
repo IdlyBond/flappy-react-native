3080f9ffb9cefec2b817071e6d32eb43b342bf43
IdlyBond <53187706+IdlyBond@users.noreply.github.com>
2023-08-03 21:15:49 +0100

    feat: physics, click & refactor

--- App.tsx
+++ App.tsx
@@ -1,32 +1,81 @@
-import React, {useEffect, useState} from 'react'
-import {StyleSheet, Text, View, ImageBackground, Image, Dimensions} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    ImageBackground,
+    Image,
+    Dimensions,
+    TouchableWithoutFeedback,
+} from 'react-native';
 export default function App() {
+    const backgroundImage = require('./assets/background.png');
 
-
-    const [characterPosition, setCharacterPosition] =  useState(0)
+    const [characterPosition, setCharacterPosition] =  useState(0);
     
-    useEffect(() => {
-        let pos = 0
+    const onButtonPressRef = useRef(null);
+    
+    useEffect(() => 
+    {
+        const isOutOfWindow = function () 
+        {
+            return pos >= (Dimensions.get('window').height / 2);
+        }
+        
+        const reset = function () 
+        {
+            pos = 0;
+            gravity = 0.45;
+            force = 0;
+        }
+
+        const jump = function ()
+        {
+            force = -10;
+        }
+        
         setCharacterPosition(0)
-        const interval =  setInterval(() => {
-            pos += 1;
-            if (pos >= Dimensions.get('window').height / 2) {
-                pos = 0;
-            }
+        onButtonPressRef.current = jump;
+
+        let pos;
+        let force;
+        let gravity;
+        
+        reset()
+        
+        const interval =  setInterval(() => 
+        {
+            force += gravity;
+            pos += force;
             setCharacterPosition(pos);
-            console.log("New Iteration " + pos)
+            
+            if (isOutOfWindow())
+            {
+                reset();
+            }
         }, 30)
+        
         return () => clearInterval(interval);
     }, [])
+    
+    
+    
     return (
-        <ImageBackground
-            source={require('./assets/background.png')}
-            style={styles.backgroundImage}>
-            <Image
-                source={require('./assets/character.png')}
-                style={[styles.character, {top: characterPosition}]}
-            ></Image>
-        </ImageBackground>
+        <TouchableWithoutFeedback
+        style={styles.touchBackground}
+        onPress={onButtonPressRef.current}>
+            <ImageBackground
+                source={backgroundImage}
+                style={styles.backgroundImage}>
+                <View pointerEvents={'none'}>
+                    <Image
+                        source={require('./assets/character.png')}
+                        style={[styles.character, {top: characterPosition}]}
+                    ></Image>
+                </View>
+            </ImageBackground>
+        </TouchableWithoutFeedback>
+            
     );
 }
 
@@ -42,5 +91,11 @@ const styles = StyleSheet.create(
         width: 100,
         height: 100,
         alignSelf: "center"
-    }
+    },
+    touchBackground: {
+        flex: 1,
+        resizeMode: "cover",
+        alignContent: "center",
+        justifyContent: "center",
+    },
 });
